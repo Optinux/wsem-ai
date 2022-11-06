bc36320793451db4a07f9641453d4b6932265ce7
Optinux <tobias.eglseder2@gmail.com>
2022-11-06 16:36:00 +0100

    rc

--- ai/use_model.py
+++ ai/use_model.py
@@ -5,7 +5,7 @@ import numpy
 from keras.applications.vgg16 import (VGG16, decode_predictions,
                                       preprocess_input)
 from keras.models import load_model
-from keras.utils import img_to_array, load_img
+from keras.utils import load_img
 
 accuracy_insg = 0  # gesamte accuracy zu beginn auf null
 accuracy_plot = []  # leere plot list
@@ -18,30 +18,24 @@ list_files = []  # leere file list
 for root, dirs, files in os.walk(path):
     for file in files:
         list_files.append(os.path.join(root, file))
-
+        
 i = 1
-for name in list_files:
+for name in list_files: # für alle files im ordner
     image = load_img(  # resize and reshape entsprechend des models
         name, target_size=(256, 256))
     img = numpy.array(image)
     img = img / 255.0
     img = img.reshape(1, 256, 256, 3)
-    label = model.predict(img)
+    label = model.predict(img)  # predict
     accuracy = label[0][0]
     print(i, ". Predicte Bild... (0 -> Classic , 1 -> Modern): ", accuracy)
-
-    # insgesamte accuracy -> accuracy alle predicteten bilder zusammen
-    accuracy_insg = accuracy_insg + accuracy
-    # fürs spätere plotten eine list befüllen
-    accuracy_plot.append(accuracy)
+    accuracy_insg = accuracy_insg + accuracy # insgesamte accuracy -> accuracy aller predicteten bilder zusammen + fürs spätere plotten eine list befüllen
+    accuracy_plot.append(accuracy) 
     # print(accuracy_plot)    # debug
-
     i += 1
-
-    if i > 400:  # wie oft er predicten soll                                              !!!WICHTIG
+    if i > 20:  # wie oft er predicten soll                                              !!!WICHTIG
         i = i - 1  # -1 damit auch die richtige anzahl geteilt wird
         break
-
 print("Insgesamte Accuracy von", i, "predicteten Bildern: ", accuracy_insg / i)
 
 # Prediction als witzige Graphen plotten, zeigt Accuracy der Prediction aller predicteten Bilder in einem Graphen
